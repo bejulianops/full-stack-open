af3caffcf094e9c78d3c5a839245523ecb4ad6cd
ejulianos <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>
2023-04-05 16:53:35 +0100

    Exercise 1.1: course information, step1

--- part-01/courseinfo/src/App.js
+++ part-01/courseinfo/src/App.js
@@ -7,6 +7,8 @@
 // Content renders the parts and their number of exercises and 
 // Total renders the total number of exercises.
 
+
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -18,19 +20,51 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content 
+        part1={part1} exercises1={exercises1}  
+        part2={part2} exercises2={exercises2} 
+        part3={part3} exercises3={exercises3} />
+      <Total sum={exercises1 + exercises2 + exercises3} />
     </div>
   );
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        In '{props.part1}' part there are {props.exercises1} exercises.
+      </p>
+      <p>
+        In '{props.part2}' part there are {props.exercises2} exercises.
+      </p>
+      <p>
+        In '{props.part3}' part there are {props.exercises3} exercises.
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        The total number of exercises is {props.sum}.
+      </p>
+    </div>
+  )
 }
 
 export default App;
